2f8584553586019c744872903f599254571363ea
Tomas Haugland Spangelo <tomas@spangelo.com>
2021-02-11 13:18:01 +0100

    Visualiaztion

--- visualize.py
+++ visualize.py
@@ -1,5 +1,6 @@
 import networkx as nx
 import matplotlib.pyplot as plt
+import numpy as np
 
 from SimWorld import SimWorld
 
@@ -7,11 +8,34 @@ s = SimWorld([(1,1)], board_type="diamond")
 
 G = nx.Graph()
 
+pos = {}
+total_height = 1 + 2 * (s.board_size)
+total_width = total_height
+
+h = total_height
+w = np.ceil(total_width/2)
+
+
+
+color_map = []
+node_size  = []
 for i in range(s.board_size):
+    h = total_height - i
+    w = np.ceil(total_width/2) - i
     for j in range(s.board_size):
         node = s.state.grid[i, j]
         if node:
             G.add_node(node)
+            pos[node] = (w, h)
+            if node.filled:
+                color_map.append('blue')
+                node_size.append(200)
+            else:
+                color_map.append('grey')
+                node_size.append(200)
+
+            h -= 1
+            w += 1
 
 for i in range(s.board_size):
     for j in range(s.board_size):
@@ -27,8 +51,9 @@ for i in range(s.board_size):
 
 
 plt.plot()
-pos = nx.spring_layout(G, iterations=100)
-nx.draw(G, pos, with_labels=True, font_weight='bold')
+#pos = nx.spring_layout(G, iterations=100)
+
+nx.draw(G, pos, node_color=color_map, node_size=node_size, with_labels=False, font_weight='bold')
 
 
 plt.show()
