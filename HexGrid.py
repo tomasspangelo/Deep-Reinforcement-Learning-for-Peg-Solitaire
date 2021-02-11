ae1d2d03277947b0f0595635accb8da1f22ca8af
Tomas Haugland Spangelo <tomas@spangelo.com>
2021-02-11 13:13:25 +0100

    Added visualization for HexGrid

--- HexGrid.py
+++ HexGrid.py
@@ -35,7 +35,8 @@ class DiamondHexGrid(HexGrid):
 
         for i in range(size):
             for j in range(size):
-                grid[i, j] = Node(i, j)
+                node = Node(i, j)
+                grid[i, j] = node
 
         for i in range(size):
             for j in range(size):
@@ -56,6 +57,60 @@ class DiamondHexGrid(HexGrid):
                 if j > 0:
                     neighborhood["left"] = grid[i, j - 1]
         return grid
+
+    @staticmethod
+    def visualize_grid(graph, state, action=None):
+        pos = {}
+        total_height = 1 + 2 * state.board_size
+        total_width = total_height
+
+        color_map = []
+        size_map = []
+
+
+        if action:
+            start_node = action.start_node
+            end_node = action.end_node
+            jump_node = action.jump_node
+
+            grid = state.state.grid
+        for i in range(state.board_size):
+            h = total_height - i
+            w = np.ceil(total_width / 2) - i
+            for j in range(state.board_size):
+                node = grid[i, j]
+                if node:
+                    graph.add_node(node)
+                    pos[node] = (w, h)
+                    if node.filled:
+                        color_map.append('blue')
+                    elif action and node.__str__() == jump_node.__str__():
+                        color_map.append('red')
+                    else:
+                        color_map.append('grey')
+
+                    if action and node.__str__() == start_node.__str__():
+                        size_map.append(200)
+
+                    elif action and node.__str__() == end_node.__str__():
+                        size_map.append(800)
+
+                    else:
+                        size_map.append(500)
+                    h -= 1
+                    w += 1
+
+        for i in range(state.board_size):
+            for j in range(state.board_size):
+                node = grid[i, j]
+                if node:
+                    for action in node.neighborhood:
+                        neighbor = node.neighborhood[action]
+                        if neighbor:
+                            graph.add_edge(node, neighbor)
+        return pos, color_map, size_map
+
+
 
 
 class TriangularHexGrid(HexGrid):
@@ -78,7 +133,8 @@ class TriangularHexGrid(HexGrid):
 
         for i in range(size):
             for j in range(i+1):
-                grid[i, j] = Node(i, j)
+                node = Node(i, j)
+                grid[i, j] = node
 
         for i in range(size):
             for j in range(i+1):
@@ -97,6 +153,57 @@ class TriangularHexGrid(HexGrid):
                     neighborhood["left"] = grid[i, j - 1]
                     neighborhood["diagonal_up"] = grid[i - 1, j - 1]
         return grid
+
+    @staticmethod
+    def visualize_grid(graph, state, action=None):
+        pos = {}
+        total_height = state.board_size
+        total_width = total_height * 2 - 1
+
+        color_map = []
+        size_map = []
+
+        if action:
+            start_node = action.start_node
+            end_node = action.end_node
+            jump_node = action.jump_node
+
+            grid = state.state.grid
+        for i in range(state.board_size):
+            h = total_height - i
+            w = np.ceil(total_width / 2) - i
+            for j in range(i+1):
+                node = grid[i, j]
+                if node:
+                    graph.add_node(node)
+                    pos[node] = (w, h)
+                    if node.filled:
+                        color_map.append('blue')
+                    elif action and node.__str__() == jump_node.__str__():
+                        color_map.append('red')
+                    else:
+                        color_map.append('grey')
+
+                    if action and node.__str__() == start_node.__str__():
+                        size_map.append(200)
+
+                    elif action and node.__str__() == end_node.__str__():
+                        size_map.append(800)
+
+                    else:
+                        size_map.append(500)
+
+                    w += 2
+
+        for i in range(state.board_size):
+            for j in range(i+1):
+                node = grid[i, j]
+                if node:
+                    for action in node.neighborhood:
+                        neighbor = node.neighborhood[action]
+                        if neighbor:
+                            graph.add_edge(node, neighbor)
+        return pos, color_map, size_map
 
 
 class Node:
@@ -119,10 +226,11 @@ class Node:
         return self.filled
 
     def __str__(self):
-        return "({row}, {column}) {filled}".format(row=self.row, column=self.column, filled=self.filled)
+        return "({row}, {column})".format(row=self.row, column=self.column, filled=self.filled)
 
 
 if __name__ == "__main__":
+    '''
     d = DiamondHexGrid(4)
     a, b = (0, 0)
     print(d.grid[a,b].neighborhood)
@@ -136,6 +244,7 @@ if __name__ == "__main__":
         print("______________________")
 
     print(d.flatten())
+    '''
 
 
 
