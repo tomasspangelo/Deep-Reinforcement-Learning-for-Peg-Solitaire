b307d5e89b86f5a639493f70536332efdf5ba616
Tomas Haugland Spangelo <tomas@spangelo.com>
2021-02-11 13:14:07 +0100

    Implemented NNCritic

--- learner.py
+++ learner.py
@@ -19,15 +19,20 @@ class Learner:
 
         actor = self.actor
         critic = self.critic
+        critic.discount_factor = self.discount_factor # TODO: Individual for actor and critic?
+        critic.decay_factor = self.decay_factor
         simworld = self.simworld
 
         remaining_pegs = []
 
         for e in range(episodes):
-            print("epiosde {e}".format(e=e))
+            print("Epiosde {e}/{episodes}".format(e=e+1, episodes=episodes))
             episode = []
 
-            simworld.reset_game()
+            if e == episodes-1:
+                simworld.reset_game(visualize=False)
+            else:
+                simworld.reset_game()
             episode_running = True
 
             actor.reset_eligibilities()
@@ -39,11 +44,14 @@ class Learner:
             if action is None:
                 raise ValueError("The game doesn't have any legal moves.")
 
-            episode.append((state, action.__str__()))
+            episode.append((state, action))
+
             while episode_running:
+
                 # Set eligibilities to 1
                 actor.set_eligibility(state, action)
-                critic.set_eligibility(state)
+                if isinstance(critic, TableCritic):
+                    critic.set_eligibility(state)
 
                 # Perform action, receive reward
                 r = actor.perform_action(action)
@@ -54,12 +62,17 @@ class Learner:
                 # Best action for new state according to policy
                 new_action = actor.get_action(new_state)
 
-                td_error = r + self.discount_factor * critic.value(new_state) - critic.value(state)
+                target = r + self.discount_factor * critic.value(new_state)
+                if r == 50 or r == -50:
+                    print("The target is {target}".format(target=target))
+                td_error = target - critic.value(state)
 
                 for (state, action) in episode:
-                    critic.update_value(state, td_error)
-
-                    critic.update_eligibility(state, self.discount_factor, self.decay_factor)
+                    if isinstance(critic, TableCritic):
+                        critic.update_value(state, td_error)
+                        critic.update_eligibility(state, self.discount_factor, self.decay_factor)
+                    else:
+                        critic.update_value(state, td_error, target)
 
                     actor.update_policy(state, action, td_error)
 
@@ -67,15 +80,16 @@ class Learner:
 
                 state = new_state
                 action = new_action
-                episode.append((state, action.__str__()))
+                episode.append((state, action))
 
                 if simworld.is_finished():
                     episode_running = False
                     actor.epsilon *= self.epsilon_decay_factor
                     remaining_pegs.append(simworld.remaining_pegs)
-                    #print(simworld.remaining_pegs)
-                    #print(simworld.flatten_state())
-                   # print("------------")
+                    print("Remaining pegs: {remaining}".format(remaining=simworld.remaining_pegs))
+                    print(critic.value(episode[len(episode)-1][0]))
+
+                    simworld.visualize_game()
         return remaining_pegs
 
 
@@ -131,7 +145,7 @@ class Actor:
 
 
 # Critic = TableCritic
-class Critic:
+class TableCritic:
 
     def __init__(self, learning_rate):
         self.eligibilities = {}
@@ -158,32 +172,43 @@ class Critic:
         self.eligibilities[np.array_str(state)] = val
 
 
-class TableCritic(Critic):
-
-    def __init__(self):
-        pass
-
-
-class NNCritic(Critic):
-
-    def __init__(self):
-        pass
-
-
 # TODO: How to get the TD error
 # TODO: Learning rate, where is it passed?
-class Network(SplitGD):
+class NNCritic(SplitGD):
 
     def __init__(self, keras_model):
         super().__init__(keras_model)
-        self.td_error = 0.00000000000000001
-        self.eligibility = []
+        self.td_error = 0
+        self.eligibilities = []
 
     def modify_gradients(self, gradients):
-        if not self.eligibility:
-            self.eligibility = np.zeros(gradients.shape)
+        # print(gradients[4].shape)
+        if not self.eligibilities:
+            for gradient_layer in gradients:
+                self.eligibilities.append(np.zeros(gradient_layer.shape))
 
-        v_grad = -1 / self.td_error * gradients
-        self.eligibility += v_grad
+        for i in range(len(gradients)):
+            gradient_layer = gradients[i]
+            v_grad = -1 /(2 * self.td_error) * gradient_layer
+            self.eligibilities[i] = self.discount_factor * self.decay_factor * self.eligibilities[i] + v_grad
+        # self.td_error * self.eligibilities
+        # print(self.eligibilities)
+        return [-self.td_error * eligibility for eligibility in self.eligibilities]
 
-        return self.td_error * self.eligibility
+    def value(self, state):
+        nn_input = state.reshape((-1, len(state)))
+        return self.model.predict(nn_input)
+
+    def update_value(self, state, td_error, target):
+        self.td_error = td_error[-1][-1]
+        nn_input = state.reshape((-1, len(state)))
+        self.fit(nn_input, target, epochs=1, verbosity=0)
+
+    def reset_eligibilities(self):
+        self.eligibilities = []
+
+
+
+if __name__ =="__main__":
+    pass
+
